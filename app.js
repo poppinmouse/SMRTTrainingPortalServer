4cff6a9bbb0d642609ba44b8108374d8f9c023a4
poppinmouse <1.kg.kywat@gmail.com>
2019-11-23 11:46:52 +0800

    Express server created. Handle post request and save to database.

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
+const app = require('express')();
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 mongoose.connect("mongodb://localhost:27017/trainingPortalDB",{useNewUrlParser: true, useUnifiedTopology: true})
 .then(() => console.log("Successfully connected to database"))
@@ -13,11 +17,10 @@ const traineeSchema = new mongoose.Schema({
 
 const Trainee = mongoose.model("Trainee", traineeSchema);
 
-
 app.post("/", (req, res) => {
     const traineeName = req.body.Name;
     const traineeId = req.body.Id;
-    const traineeInterchange = req.body.Interchange
+    const traineeInterchange = req.body.Interchange;
 
     const trainee = new Trainee({
         name: traineeName,
@@ -31,4 +34,6 @@ app.post("/", (req, res) => {
 
     console.log("New user info saved");
 });
+
+app.listen(3000, () => console.log("Server Started Successfully"));
 
