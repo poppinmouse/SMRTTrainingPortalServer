263a8609fc10e30a0b8f9639823e981b85953cd7
poppinmouse <1.kg.kywat@gmail.com>
2019-12-09 15:01:39 +0800

    email sent successfully.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const app = require('express')();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const nodemailer = require('nodemailer');
 
 app.use(bodyParser.urlencoded({extended: true}));
 
@@ -215,6 +216,37 @@ app.route("/bookings/:bookingId/bookeddate")
         }
     )
 });
+
+
+app.route("/email")
+.post((req, res) => {
+    
+    var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+        user: 'testnodemailer123456@gmail.com',
+        pass: 'TestNodemailer123'
+        }
+    });
+    
+    const email = JSON.parse(req.body.Email);
+
+    var mailOptions = {
+        from: 'testnodemailer123456@gmail.com',
+        to: email.interchangeAddress + "," + email.trainerAddress,
+        subject: email.subject,
+        text: email.body       
+    };
+    
+    transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+        console.log(error);
+        } else {
+        console.log('Email sent: ' + info.response);
+        }
+    });
+});
+
 
 app.listen(3000, () => console.log("Server Started Successfully"));
 
